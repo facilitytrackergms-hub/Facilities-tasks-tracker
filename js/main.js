9fc506c69763863a72fde083915ff99e22ec7ca7
facilitytrackergms-hub <facilitytracker.gms@gmail.com>
2026-06-21 02:45:44 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-/* Purpose: Responsive Card Styling | Location: /styles/main.css */
-body {
-    margin: 0;
-    padding: 0;
-    width: 100%;
-}
 
-.card {
-    border: 2px solid #ccc;
-    border-radius: 10px;
-    padding: 15px;
-    margin: 10px auto; 
-    width: 90%;
-    max-width: 500px;
-    box-sizing: border-box;
-    background-color: #f9f9f9;
-}
